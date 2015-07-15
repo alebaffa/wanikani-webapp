ebaea018ff26f48ba26e2f355afb1fdc78b42452
Alessandro BAFFA <alessandro.baffa@amadeus.com>
2015-07-15 13:24:23 +0200

    call different levels of the kanji API

--- main.go
+++ main.go
@@ -8,9 +8,29 @@ import (
 
 func main() {
 
-	user := apiutils.GetKanjiList("1")
-	for _, character := range user.RequestedInfo {
-		fmt.Printf("%#U", character.Character)
+	kanjin1 := apiutils.GetKanjiList("1")
+	kanjin2 := apiutils.GetKanjiList("2")
+	kanjin3 := apiutils.GetKanjiList("3")
+	kanjin4 := apiutils.GetKanjiList("4")
+
+	fmt.Println("\nKanji n1: ")
+	for _, character := range kanjin1.RequestedInfo {
+		fmt.Printf(character.Character + ", ")
+	}
+
+	fmt.Println("\nKanji n2: ")
+	for _, character := range kanjin2.RequestedInfo {
+		fmt.Printf(character.Character + ", ")
+	}
+
+	fmt.Println("\nKanji n3: ")
+	for _, character := range kanjin3.RequestedInfo {
+		fmt.Printf(character.Character + ", ")
+	}
+
+	fmt.Println("\nKanji n4: ")
+	for _, character := range kanjin4.RequestedInfo {
+		fmt.Printf(character.Character + ", ")
 	}
 
 }
